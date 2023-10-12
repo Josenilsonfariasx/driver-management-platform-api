69bb4ccbad44ff85ac3a3ac51f257f1561b1dbcc
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-11 21:41:56 -0300

    feat: adding database/seed functionality from admin

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Admin::create([
+            'name' => 'me2_soft',
+            'password' => Hash::make('me2-teste'),
+        ]);
+    }
+}
