dba8b7d2efcdf21eaaf6a84bb211e839d3354d63
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-12 13:53:10 -0300

    feat: adding route login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
-use App\Http\Controllers\DriverController;
+use App\Http\Controllers\{AuthController,DriverController};
 use Illuminate\Support\Facades\Route;
 
 Route::post('/drivers',[DriverController::class, 'create']);
+Route::post('/auth/login',[AuthController::class, 'login']);
