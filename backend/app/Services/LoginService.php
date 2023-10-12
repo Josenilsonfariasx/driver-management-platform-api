7890459d3a221055abce4ae3e65e14249d282ec9
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-12 13:52:11 -0300

    feat: service for login

---
+++ backend/app/Services/LoginService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+
+use App\Exceptions\AppError;
+use Illuminate\Support\Facades\Log;
+class LoginService {
+  public function execute(array $data){
+    Log::debug('Service login');
+
+    if(!$token = auth()->attempt($data)){
+      throw new AppError('Not authorized.', 401);
+    }
+    return $this->responseToken($token);
+  }
+  private function responseToken($token) {
+    return response()->json([
+        'token'=> $token,
+        'user'=> auth()->user()
+    ]);
+  }
+}
