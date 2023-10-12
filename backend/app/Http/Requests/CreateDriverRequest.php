3747b333c5eaeda54d375cd6e46612dff7081194
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-12 08:45:33 -0300

    feat: handling errors when creating driver

---
+++ backend/app/Http/Requests/CreateDriverRequest.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+class CreateDriverRequest extends FormRequest {
+    public function authorize(): bool {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array {
+        return [
+            'name'=>['required'],
+            'cpf'=>['required'],
+            'telephone'=>['required'],
+            'password'=>['required', 'min:7'],
+            
+        ];
+    }
+}
