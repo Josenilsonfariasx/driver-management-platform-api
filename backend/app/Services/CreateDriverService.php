db85c2435d6baae3ea418e60de8a3f80cfd180a3
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-12 08:44:15 -0300

    feat: adding services for driver and validation for existing driver

---
+++ backend/app/Services/CreateDriverService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+
+use App\Exceptions\AppError;
+use App\Models\Driver;
+class CreateDriverService {
+  public function execute(array $data){
+
+    $CpfFound = Driver::firstWhere('cpf', $data['cpf']);
+    if(!is_null($CpfFound)){
+      throw new AppError('There is already a person with this CPF.',400);
+    }
+
+    $CellFound = Driver::firstWhere('telephone', $data['telephone']);
+    if(!is_null($CellFound)){
+      throw new AppError('There is already a person with this number from telephone.',400);
+    }
+
+    return Driver::create($data);
+  }
+}
