2409ea3848f63e4115abf6d2948a069058de0598
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-13 17:00:42 -0300

    feat: adding routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\{AuthController, DeleteDriverController, DeleteTransportController, DriverController, EditDriverController, EditTransportController, GetAllDriversController, GetInforDriverByIdController, TransportController};
+use App\Http\Controllers\{AuthController, DeleteDriverController, DeleteTransportController, DriverController, EditDriverController, EditTransportController, GetAllDriversController, GetDriverByCpfController, GetDriverByNameController, GetInforDriverByIdController, TransportController};
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login',[AuthController::class, 'login']);
@@ -8,6 +8,9 @@ route::middleware('jwt.verify')->group(function() {
   // GET
   Route::get('/drivers',[GetAllDriversController::class, 'read']);
   Route::get('/drivers/{id}/info', [GetInforDriverByIdController::class, 'getDriverById']);
+  Route::get('/drivers/search/{name}', [GetDriverByNameController::class, 'getDriverByName']);
+  Route::get('/drivers/search/cpf/{cpf}', [GetDriverByCpfController::class, 'getDriverByCpf']);
+
   // POST
   Route::post('/drivers',[DriverController::class, 'create']);
   Route::post('/transport',[TransportController::class, 'create']);
