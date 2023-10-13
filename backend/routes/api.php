964953b8ba0c9b81b27f7c6131f14065a540bb95
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-13 13:11:13 -0300

    feat: adding routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\{AuthController,DriverController, GetAllDriversController, GetInforDriverByIdController, TransportController};
+use App\Http\Controllers\{AuthController,DriverController, EditDriverController, EditTransportController, GetAllDriversController, GetInforDriverByIdController, TransportController};
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login',[AuthController::class, 'login']);
@@ -11,4 +11,7 @@ route::middleware('jwt.verify')->group(function() {
   // POST
   Route::post('/drivers',[DriverController::class, 'create']);
   Route::post('/drivers/transport',[TransportController::class, 'create']);
+  // PUT
+  Route::put('/drivers/{id}', [EditDriverController::class, 'edit']);
+  Route::put('/transport/{id}', [EditTransportController::class, 'edit']);
 });
