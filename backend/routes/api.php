0370147627a97c6533434e154432796948175d91
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-13 08:56:03 -0300

    feat: adding routes getAllDriver getDriverInfo

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,14 @@
 <?php
-use App\Http\Controllers\{AuthController,DriverController,TransportController};
+use App\Http\Controllers\{AuthController,DriverController, GetAllDriversController, GetInforDriverByIdController, TransportController};
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login',[AuthController::class, 'login']);
 
 route::middleware('jwt.verify')->group(function() {
+  // GET
+  Route::get('/drivers',[GetAllDriversController::class, 'read']);
+  Route::get('/drivers/{id}/info', [GetInforDriverByIdController::class, 'getDriverById']);
+  // POST
   Route::post('/drivers',[DriverController::class, 'create']);
   Route::post('/drivers/transport',[TransportController::class, 'create']);
 });
